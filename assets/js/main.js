8645e16a02d5b526c718dbcc3fcc89650022c10e
Enik Jhony <enick.jhony@gmail.com>
2022-07-31 13:36:33 -0400

    Finalizando a section testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,24 @@ function activeWork(){
 linkWork.forEach(l=> l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    spaceBeteween: 24,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+    },
+    breakpoints:{
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBeteween: 48,
+        }
+    }
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
